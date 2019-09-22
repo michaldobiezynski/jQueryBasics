aadb286105b28503abbd3768485592350c2ba374
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-09-22 14:08:03 +0100

    made it run

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+//
+//const title = "My First Blog Post";
+//const content = "This is my <strong>first</strong> post!";
+//
+//$('#blogTitlePreview').text(title);
+//$('#blogContentPreview').html(content);
+//
+ $('#flashMessage')
+.hide()
+
+
+$('#previewButton').click(function() {
+  
+  const title = $('#blogTitleInput').val();
+  const content = $('#blogContentInput').val();
+  
+  $('#blogTitlePreview').text(title);
+  $('#blogContentPreview').html(content);
+
+  $('#flashMessage')
+  .slideDown(1000)
+  .delay(3000)
+  .slideUp();
+  
+  
+  });
